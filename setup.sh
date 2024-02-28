650fb0d66cbd0b599bafd35fa978aaf688523c85
Anthony <oips.anthony@gmail.com>
2024-02-28 14:56:04 +0800

    feat: update readme.md and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
+# backup config
 mv ~/.config/nvim ~/.config/nvim.bak
 mv ~/.local/share/nvim ~/.local/share/nvim.bak
 mv ~/.local/state/nvim ~/.local/state/nvim.bak
 mv ~/.cache/nvim ~/.cache/nvim.bak
 
 # clone code
-git clone https://github.com/chaozwn/astronvim_with_coc_or_mason ~/.config/nvim
+git clone https://github.com/OiAnthony/AstroNvimConfig ~/.config/nvim
 
+# install dependencies
 brew install fzf
 brew install luarocks
 brew install lazygit
 brew install ripgrep
-npm install -g tree-sitter-cli
+pnpm add -g tree-sitter-cli
 brew install gdu
 brew install bottom
 brew install protobuf
 
-pip install git+https://github.com/will8211/unimatrix.git
-npm install -g neovim
-pip install pynvim
+pip3 install git+https://github.com/will8211/unimatrix.git
+pnpm add -g neovim
+pip3 install pynvim
 
 brew tap daipeihust/tap
 brew install im-select
 
-npm i -g @styled/typescript-styled-plugin typescript-styled-plugin
+pnpm add -g @styled/typescript-styled-plugin typescript-styled-plugin
 
-npm i -g typescript-vue-plugin
-npm i -g vue-component-meta
+pnpm add -g typescript-vue-plugin
+pnpm add -g vue-component-meta
 
 brew install neovide
 brew install lazydocker
